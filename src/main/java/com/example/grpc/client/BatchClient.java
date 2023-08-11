fe1115e0e30dfe879aea5fba90d89b228f223ea0
Christopher Razavian <pooya72@gmail.com>
2023-08-11 18:27:00 +0100

    Client should consume list of batches

--- src/main/java/com/example/grpc/client/BatchClient.java
+++ src/main/java/com/example/grpc/client/BatchClient.java
@@ -3,18 +3,28 @@ package com.example.grpc.client;
 import com.example.grpc.BatchResponse;
 import com.example.grpc.BatchServiceGrpc;
 import com.example.grpc.CreateBatchRequest;
+import com.example.grpc.ListRequest;
+import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Iterator;
+
 public class BatchClient {
+
+    public static final ManagedChannel CHANNEL =
+            ManagedChannelBuilder.forAddress("localhost", 6565).usePlaintext().build();
+
     public BatchResponse create() {
-        final var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
-                .usePlaintext()
-                .build();
         final var request = CreateBatchRequest.newBuilder()
                 .setBatchId("batchId")
                 .setSku("sku")
                 .setSupplierId("supplierId")
                 .build();
-        return BatchServiceGrpc.newBlockingStub(channel).create(request);
+        return BatchServiceGrpc.newBlockingStub(CHANNEL).create(request);
+    }
+
+    public Iterator<BatchResponse> list() {
+        final var request = ListRequest.newBuilder().build();
+        return BatchServiceGrpc.newBlockingStub(CHANNEL).list(request);
     }
 }
--- src/test/java/com/example/grpc/client/BatchClientTest.java
+++ src/test/java/com/example/grpc/client/BatchClientTest.java
@@ -1,6 +1,6 @@
 package com.example.grpc.client;
 
-import com.example.grpc.CreateBatchResponse;
+import com.example.grpc.BatchResponse;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,12 +9,23 @@ class BatchClientTest {
 
     @Test
     void create() {
-        final var expected = CreateBatchResponse.newBuilder()
+        final var expected = BatchResponse.newBuilder()
                 .setBatchId("batchId")
                 .setSku("sku")
                 .setSupplierId("supplierId")
                 .build();
         final var actual = new BatchClient().create();
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void list() {
+        final var expected = BatchResponse.newBuilder()
+                .setBatchId("batchId")
+                .setSku("sku")
+                .setSupplierId("supplierId")
+                .build();
+        final var actual = new BatchClient().list();
+        assertThat(actual.next()).isEqualTo(expected);
     }
 }
